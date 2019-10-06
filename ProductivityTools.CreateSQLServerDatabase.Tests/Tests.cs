7d892bd94d121e13502c07d6233b4dc9d04ad336
Pawel Wujczyk <pwujczyk@hotmail.com>
2019-10-06 22:13:28 +0200

    implementation

---
+++ ProductivityTools.CreateSQLServerDatabase.Tests/Tests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ProductivityTools.CreateSQLServerDatabase.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIfDatabaseExists()
+        {
+            string name="DB"+ Path.GetRandomFileName().Replace('.','x');
+            Database database = new Database(name, "Server=.\\SQL2017;Trusted_Connection=True;");
+            var r=database.Exists();
+            Assert.IsFalse(r);
+
+            database.Create();
+            r = database.Exists();
+            Assert.IsTrue(r);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SqlException))]
+        public void TryCreateDatabaseTwice()
+        {
+            string name = "DB" + Path.GetRandomFileName().Replace('.', 'x');
+            Database database = new Database(name, "Server=.\\SQL2017;Trusted_Connection=True;");
+            database.Create();
+            database.Create();
+        }
+    }
+}
--- ProductivityTools.CreateSQLServerDatabase/Database.cs
+++ ProductivityTools.CreateSQLServerDatabase/Database.cs
@@ -8,30 +8,50 @@ namespace ProductivityTools.CreateSQLServerDatabase
         private readonly string Name;
         private readonly string DataSourceConnectionString;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Name of the database</param>
+        /// <param name="dataSourceConnectionString">Connection string which has server name and authentication method like Server=.\\SQL2017;Trusted_Connection=True;</param>
         public Database(string name, string dataSourceConnectionString)
         {
             this.Name = name;
             this.DataSourceConnectionString = dataSourceConnectionString;
         }
 
+        /// <summary>
+        /// Creates database, if exists throws exception
+        /// </summary>
         public void Create()
         {
             InvokeQuery($"CREATE DATABASE {this.Name}");
         }
 
+        /// <summary>
+        /// Create database if not exists if exists it to nothing
+        /// </summary>
+        public void CreateSilent()
+        {
+            if (Exists()==false)
+            {
+                Create();
+            }
+        }
 
+        /// <summary>
+        /// Check if database exists
+        /// </summary>
+        /// <returns></returns>
         public bool Exists()
         {
-            InvokeReader($"SELECT [Name] FROM sys.databases WHERE [name] = '{this.Name}'");
-            return true;
+            var result=InvokeReader($"SELECT Count(1) FROM sys.databases WHERE [name] = '{this.Name}'");
+            return result == "1";
         }
 
         private void InvokeQuery(string query)
         {
             InvokeQuery(this.DataSourceConnectionString, query);
         }
-
-
 
         private void InvokeQuery(string connectionString, string query)
         {
@@ -43,9 +63,10 @@ namespace ProductivityTools.CreateSQLServerDatabase
             }
         }
 
-        private void InvokeReader(string query)
+        private string InvokeReader(string query)
         {
-            InvokeReader(this.DataSourceConnectionString, query);
+            var result=InvokeReader(this.DataSourceConnectionString, query);
+            return result;
         }
 
         private string InvokeReader(string connectionString, string query)
